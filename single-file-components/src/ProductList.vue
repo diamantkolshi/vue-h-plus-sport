a19d020c66a2c26670433c8ea440d68c555c7778
Diamant <diamantkolshi@gmail.com>
2017-05-22 21:41:20 +0200

    add single-file-components with webpack

---
+++ single-file-components/src/ProductList.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="product-list">
+    <h2>{{title}}</h2>
+    <ul>
+      <product-list-item v-for="(product, i) in products" @remove="remove(i)" :product="product">
+      </product-list-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+	import ProductListItem from './ProductListItem.vue';
+
+	export default {
+		name: "product-list",
+		props: ['products', 'title'],
+        methods: {
+            remove: function(i) {
+              this.products.splice(i, 1);
+            }
+        } 
+	};
+
+</script>
+
+<style scoped>
+  h2 {
+    margin-bottom: 40px;
+  }
+
+  ul {
+    list-style-type: none;
+  }
+</style> 
---
+++ single-file-components/src/ProductListItem.vue
@@ -0,0 +1,32 @@
+<template>
+	<li>
+     <img :src="product.image">
+    <p><strong>{{product.name}}</strong></p>
+    <p>description of {{product.name}} </p>
+    <a @click="requestRemoval">Hide this item</a>
+  </li>
+</template>
+
+<script type="text/javascript">
+ export default {
+ 	name: 'product-list-item',
+ 	props: ['product'],
+    methods: { 
+      requestRemoval: function() {
+        this.$emit('remove')
+      }
+    }
+ };
+</script>
+
+<style scoped>
+    img {
+        float: left;
+        width: 300px;
+    }
+    li {
+        margin-bottom: 40px;
+        clear: both;
+    }
+
+</style>       
